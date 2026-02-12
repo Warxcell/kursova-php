cdf28b9bbe17ff2ce841d505a67c4b8c8b16f38d
Bozhidar Hristov <warxcell@gmail.com>
2026-02-12 20:07:03 +0200

    style empty state

--- templates/contact_admin.php
+++ templates/contact_admin.php
@@ -8,33 +8,36 @@ $this->layout('layout', ['title' => 'Запитвания', 'currentAdmin' => $c
 <?php $this->end() ?>
 
 <div class="enquiries-list">
-    <?php
-    foreach ($enquiries as $enquiry): ?>
-        <div class="enquiry-card">
-            <div class="enquiry-header">
-                <span class="enquiry-id">#<?= $this->e($enquiry['id']) ?></span>
-                <span class="enquiry-name"><?= $this->e($enquiry['name']) ?></span>
-                <span class="enquiry-email"><?= $this->e($enquiry['email']) ?></span>
-            </div>
+    <?php if (count($enquiries) > 0): ?>
+        <?php foreach ($enquiries as $enquiry): ?>
+            <div class="enquiry-card">
+                <div class="enquiry-header">
+                    <span class="enquiry-id">#<?= $this->e($enquiry['id']) ?></span>
+                    <span class="enquiry-name"><?= $this->e($enquiry['name']) ?></span>
+                    <span class="enquiry-email"><?= $this->e($enquiry['email']) ?></span>
+                </div>
 
-            <div class="enquiry-text">
-                <?= $this->e($enquiry['text']) ?>
-            </div>
+                <div class="enquiry-text">
+                    <?= $this->e($enquiry['text']) ?>
+                </div>
 
-            <div class="enquiry-actions">
-                <?php
-                if (filter_var($enquiry['handled'], FILTER_VALIDATE_BOOL)): ?>
-                    <span class="enquiry-status handled">✓ Обработено</span>
-                <?php
-                else: ?>
-                    <form class="enquiry-form">
-                        <button type="submit" name="id" value="<?= $this->e($enquiry['id']) ?>">Отбележи като обработено
-                        </button>
-                    </form>
-                <?php
-                endif; ?>
+                <div class="enquiry-actions">
+                    <?php
+                    if (filter_var($enquiry['handled'], FILTER_VALIDATE_BOOL)): ?>
+                        <span class="enquiry-status handled">✓ Обработено</span>
+                    <?php
+                    else: ?>
+                        <form class="enquiry-form">
+                            <button type="submit" name="id" value="<?= $this->e($enquiry['id']) ?>">Отбележи като
+                                обработено
+                            </button>
+                        </form>
+                    <?php
+                    endif; ?>
+                </div>
             </div>
-        </div>
-    <?php
-    endforeach; ?>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <div class="empty-state">Няма запитвания</div>
+    <?php endif; ?>
 </div>
